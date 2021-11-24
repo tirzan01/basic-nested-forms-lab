b136badc2bd44ebbd716c6562eb91b59aed41639
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-24 22:35:47 +1100

    done with assignment

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: '1 grams')
+    @recipe.ingredients.build(quantity: '1 grams')
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
